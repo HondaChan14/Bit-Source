83955f21a9f11b89972555452f850c932f0089d2
HondaChan14 <92832561+HondaChan14@users.noreply.github.com>
2023-03-12 20:18:33 -0400

    feat: Issueboard and card components added

--- client/src/Components/Home/LandingPage.jsx
+++ client/src/Components/Home/LandingPage.jsx
@@ -5,11 +5,21 @@ import IssueBoard from '../IssueBoard/IssueBoard';
 import MobileNav from '../Header/MobileNav';
 
 const LandingPage = () => {
+
+  const issues = [
+    { id: 1, title: 'First issue', description: 'This is the first issue.' },
+    { id: 2, title: 'Second issue', description: 'This is the second issue.' },
+    { id: 3, title: 'Third issue', description: 'This is the third issue.' },
+    { id: 4, title: 'Fourth issue', description: 'This is the fourth issue.' },
+    { id: 5, title: 'Fifth issue', description: 'This is the fifth issue.' },
+    { id: 6, title: 'Sixth issue', description: 'This is the sixth issue.' },
+  ];
+
   return (
-    <div>
+    <div className="bg-red-400 min-h-screen">
       <NavBar />
       <MobileNav />
-      <IssueBoard />
+      <IssueBoard issues={issues}/>
       <Footer />
     </div>
   )
---
+++ client/src/Components/IssueBoard/GoodFirstIssueCard.jsx
@@ -0,0 +1,18 @@
+const GoodFirstIssueCard = ({issue}) => {
+    return (
+          <div className="rounded-2xl bg-gradient-to-r from-sky-400 to-blue-500 p-1 shadow-xl">
+            <div className="block rounded-xl bg-rose-50 p-4 sm:p-6 lg:p-8">
+              <h2 className="text-lg font-medium mb-2">{issue.title}</h2>
+              <span>Created Date: {issue.created_date}</span>
+            <div className="mt-2 border-t-2 border-gray-200">
+              <p className="text-gray-700 mt-2 mb-4 h-64">Description: {issue.description}</p>
+              <button className=" bg-slate-300 hover:bg-slate-700 text-white font-bold py-2 px-4 rounded">
+                <a href={issue.url} target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:text-blue-800 hover:underline cursor-pointer">View on Github</a>
+              </button>
+            </div>
+            </div>
+          </div>
+    )
+  }
+  
+  export default GoodFirstIssueCard
