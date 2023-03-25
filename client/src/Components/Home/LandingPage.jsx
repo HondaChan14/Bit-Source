120842a64ace14a0d219ec515e5891448e294ef4
HondaChan14 <92832561+HondaChan14@users.noreply.github.com>
2023-03-25 11:00:10 -0400

    feat(client): Pass data from Github API from server to client components

--- client/src/Components/Home/LandingPage.jsx
+++ client/src/Components/Home/LandingPage.jsx
@@ -1,28 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import NavBar from '../Header/Navbar';
 import Footer from '../Footer/Footer';
 import IssueBoard from '../IssueBoard/IssueBoard';
 import MobileNav from '../Header/MobileNav';
 
 const LandingPage = () => {
+    const [issues, setIssues] = useState([]);
 
-  const issues = [
-    { id: 1, title: 'First issue', description: 'This is the first issue.' },
-    { id: 2, title: 'Second issue', description: 'This is the second issue.' },
-    { id: 3, title: 'Third issue', description: 'This is the third issue.' },
-    { id: 4, title: 'Fourth issue', description: 'This is the fourth issue.' },
-    { id: 5, title: 'Fifth issue', description: 'This is the fifth issue.' },
-    { id: 6, title: 'Sixth issue', description: 'This is the sixth issue.' },
-  ];
+    useEffect(() => {
+        const fetchIssues = async () => {
+            try {
+                const response = await fetch('http://localhost:8000/');
+                console.log(`resp`, response);
+                const data = await response.json();
+                console.log(`data`, data);
+                setIssues(data);
+            } catch (error) {
+                console.error(`${error} is coming from LandingPage`);
+            }
+        };
+        fetchIssues();
+    }, []);
 
-  return (
-    <div className="bg-red-400 min-h-screen">
-      <NavBar />
-      <MobileNav />
-      <IssueBoard issues={issues}/>
-      <Footer />
-    </div>
-  )
-}
+    return (
+        <div className="bg-red-400 min-h-screen">
+            <NavBar />
+            <MobileNav />
+            <IssueBoard issues={issues} />
+            <Footer />
+        </div>
+    );
+};
 
-export default LandingPage
+export default LandingPage;
