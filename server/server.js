1eb335943037e28ee0b01864020f63f448ae050d
HondaChan14 <92832561+HondaChan14@users.noreply.github.com>
2023-03-25 10:57:29 -0400

    feat(server): Update server add route/controller

--- server/server.js
+++ server/server.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const app = express();
-const mongoose = require('mongoose');
-
+const cors = require('cors');
+//const mongoose = require('mongoose');
 //Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI)
+//mongoose.connect(process.env.MONGODB_URI)
+
+const mainRoutes = require('./routes/main');
+
+require('dotenv').config({ path: './config/.env' });
+
+app.use((req, res, next) => {
+  res.setHeader('Content-Type', 'application/json');
+  next();
+});
+
+const corsOptions = {
+  origin: `${process.env.Origin}`,
+};
+
+app.use(cors(corsOptions));
+
+app.use('/', mainRoutes);
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
-})
+  console.log(`Server running on port ${process.env.PORT}`);
+});
