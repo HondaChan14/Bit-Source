c35563b40e5fc0b70e2dcc5d3e407a3d74922413
HondaChan14 <92832561+HondaChan14@users.noreply.github.com>
2023-04-03 17:52:48 -0400

    feat: Updated formatting of files

--- client/src/Components/Header/NavBar.jsx
+++ client/src/Components/Header/NavBar.jsx
@@ -1,30 +1,34 @@
-import { React } from 'react'
-import { GiFallingStar } from "react-icons/gi";
+import { React } from 'react';
+import { GiFallingStar } from 'react-icons/gi';
 
 const NavBar = () => {
-  
-  return (
-    <header className='top-0 sticky hidden tablet:block bg-slate-800 text-white py-6'>
-        <nav className='container flex items-center justify-between mx-auto'>
-          <a href="http://" className='flex items-center'>
-            <span className='flex uppercase self-center text-logo font-bold text-3xl tracking-wider'>
-              Bit Source <GiFallingStar className='text-3xl self-center' />
-            </span>
-          </a>
+    return (
+        <header className="top-0 sticky hidden tablet:block bg-slate-800 text-white py-6">
+            <nav className="container flex items-center justify-between mx-auto">
+                <a href="http://" className="flex items-center">
+                    <span className="flex uppercase self-center text-logo font-bold text-3xl tracking-wider">
+                        Bit Source{' '}
+                        <GiFallingStar className="text-3xl self-center" />
+                    </span>
+                </a>
 
-            <div className='md:flex md:items-center'>
-              <ul className='flex items-center text-xl'>
-                <li className='mx-4'>
-                <a href="#" className='text-white'>Log In</a>
-                </li>
-                <li className='mx-4'>
-                  <a href="#" className='text-white'>Sign Up</a>
-                </li>
-              </ul>
-            </div>
-        </nav>
-    </header>
-  )
-}
+                <div className="md:flex md:items-center">
+                    <ul className="flex items-center text-xl">
+                        <li className="mx-4">
+                            <a href="#" className="text-white">
+                                Log In
+                            </a>
+                        </li>
+                        <li className="mx-4">
+                            <a href="#" className="text-white">
+                                Sign Up
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+    );
+};
 
-export default NavBar
+export default NavBar;
