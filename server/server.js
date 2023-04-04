65ffed92e4174bd0feb19e3e7822a23132f6e295
HondaChan14 <92832561+HondaChan14@users.noreply.github.com>
2023-04-03 20:06:24 -0400

    feat(server) Updated server to ES6 module syntax

--- server/server.js
+++ server/server.js
@@ -1,21 +1,21 @@
-const express = require('express');
+import express from 'express';
 const app = express();
-const cors = require('cors');
+import cors from 'cors';
 //const mongoose = require('mongoose');
 //Connect to MongoDB
 //mongoose.connect(process.env.MONGODB_URI)
 
-const mainRoutes = require('./routes/main');
+import mainRoutes from './routes/main';
 
 require('dotenv').config({ path: './config/.env' });
 
 app.use((req, res, next) => {
-  res.setHeader('Content-Type', 'application/json');
-  next();
+    res.setHeader('Content-Type', 'application/json');
+    next();
 });
 
 const corsOptions = {
-  origin: `${process.env.Origin}`,
+    origin: `${process.env.Origin}`,
 };
 
 app.use(cors(corsOptions));
@@ -23,5 +23,5 @@ app.use(cors(corsOptions));
 app.use('/', mainRoutes);
 
 app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
+    console.log(`Server running on port ${process.env.PORT}`);
 });
