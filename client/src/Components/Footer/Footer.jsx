35f4db6ee07869b74e5f2e70949992ff1740733d
HondaChan14 <92832561+HondaChan14@users.noreply.github.com>
2023-04-07 22:07:17 -0400

    style(client) Updated file formatting & eslint config

--- client/src/Components/Footer/Footer.jsx
+++ client/src/Components/Footer/Footer.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from 'react';
 
 const Footer = () => {
-  return (
-    <footer className="text-center bg-slate-800 text-white py-3">
-      <span>Made by Jason Jugo and open sourced on <a href="https://github.com/" className='underline'>GitHub</a></span>
-    </footer>
-  )
-}
+    return (
+        <footer className="text-center bg-slate-800 text-white py-3">
+            <span>
+                Made by Jason Jugo and open sourced on{' '}
+                <a href="https://github.com/" className="underline">
+                    GitHub
+                </a>
+            </span>
+        </footer>
+    );
+};
 
-export default Footer
+export default Footer;
--- client/src/Components/IssueBoard/IssueBoard.jsx
+++ client/src/Components/IssueBoard/IssueBoard.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import GoodFirstIssueCard from './GoodFirstIssueCard';
 
 function IssueBoard({ issues }) {
-  return (
-    <div className="min-h-screen bg-slate-800 p-10 grid grid-cols-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
-      {issues.map(issue => (
-        <GoodFirstIssueCard key={issue.id} issue={issue} />
-      ))}
-    </div>
-  );
+    return (
+        <div className="min-h-screen bg-slate-800 p-10 grid grid-cols-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+            {issues.map((issue) => (
+                <GoodFirstIssueCard key={issue.id} issue={issue} />
+            ))}
+        </div>
+    );
 }
 
 export default IssueBoard;
