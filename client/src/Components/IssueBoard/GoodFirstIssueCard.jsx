0cb8583b8a0947c074c1651eda27641072c52d61
HondaChan14 <92832561+HondaChan14@users.noreply.github.com>
2023-05-20 08:06:48 -0400

    feat(client): Added favorite star icon and onClick fucntionality

--- client/src/Components/IssueBoard/GoodFirstIssueCard.jsx
+++ client/src/Components/IssueBoard/GoodFirstIssueCard.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { AiFillStar, AiOutlineStar } from "react-icons/ai";
 import IssueTag from './IssueTag';
 import octoCat from '/src/images/octoface.svg';
 
@@ -12,6 +13,12 @@ const GoodFirstIssueCard = ({ issue }) => {
         return label.name.split('')[0].toUpperCase() + label.name.slice(1);
     });
     const userProfile = issue.user.html_url;
+
+    const [isStarred, setIsStarred] = React.useState(false);
+
+    const handleStarClick = () => {
+        setIsStarred(!isStarred);
+    }
 
     return (
         <div className="rounded-2xl bg-gradient-to-r from-sky-400 to-blue-500 p-1 shadow-xl hover:shadow-2xl transition duration-500 transform hover:-traslate-y-1 hover:scale-105">
@@ -45,6 +52,11 @@ const GoodFirstIssueCard = ({ issue }) => {
                     {labelData.map((label) => (
                         <IssueTag key={label} issueTag={label} />
                     ))}
+                </div>
+                <div className='absolute right-8 bottom-4'>
+                    <button onClick={handleStarClick} className='text-white text-3xl font-bold'>
+                        {isStarred ? <AiFillStar/> : <AiOutlineStar/>}
+                    </button>
                 </div>
                 <div className="absolute bottom-4 rounded-md border-2 border-white-800">
                     <button
