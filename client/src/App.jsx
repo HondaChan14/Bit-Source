b766fca4d31bb0237a04cd50fcc62f7313dd56bf
HondaChan14 <92832561+HondaChan14@users.noreply.github.com>
2023-05-28 19:36:54 -0400

    Addition of Clerk for Login/Signup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,76 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom';
 import React from 'react';
 import LandingPage from './pages/Home/LandingPage';
 import NavBar from './Components/Header/NavBar';
 import MobileNav from './Components/Header/MobileNav';
 import Footer from './Components/Footer/Footer';
-import './index.css';
-import SignUp from './pages/Signup/SignUp';
-import Login from './pages/Login/Login';
 
+// import ClerkProviderWithRoutes from './Components/Clerk/ClerkProviderWithRoutes';
+
+import './index.css';
+import {
+    ClerkProvider,
+    SignedIn,
+    SignedOut,
+    RedirectToSignIn,
+    SignIn,
+    SignUp,
+} from '@clerk/clerk-react';
+
+if (!import.meta.env.VITE_REACT_APP_CLERK_PUBLISHABLE_KEY) {
+    throw new Error('Missing Publishable Key');
+}
+const clerkPubKey = import.meta.env.VITE_REACT_APP_CLERK_PUBLISHABLE_KEY;
+
+function ClerkProviderWithRoutes() {
+    const navigate = useNavigate();
+
+    return (
+        <div className='flex justify-center items-center'>
+            <ClerkProvider
+                publishableKey={clerkPubKey}
+                navigate={(to) => navigate(to)}
+            >
+                <Routes>
+                    <Route path='/' element={<LandingPage />} />
+                    <Route
+                        path='/signin'
+                        element={<SignIn routing='path' path='/signin' />}
+                    />
+                    <Route
+                        path='/signup'
+                        element={<SignUp routing='path' path='/signup' />}
+                    />
+                    <Route
+                        path='/dashboard'
+                        element={
+                            <>
+                                <SignedIn>
+                                    <LandingPage />
+                                </SignedIn>
+                                <SignedOut>
+                                    <RedirectToSignIn />
+                                </SignedOut>
+                            </>
+                        }
+                    />
+                </Routes>
+            </ClerkProvider>
+        </div>
+    );
+}
 function App() {
     return (
-        <div className="bg-slate-800">
+        <div className='bg-slate-800 h-screen'>
             <BrowserRouter>
                 <NavBar />
                 <MobileNav />
-                <div>
-                    <Routes>
-                        <Route path="/" element={<LandingPage />} />
-                        <Route path="/signup" element={<SignUp />} />
-                        <Route path="/login" element={<Login />} />
-                    </Routes>
-                </div>
+                {/* <Routes>
+                        <Route path='/' element={<LandingPage />} />
+                        <Route path='/signup' element={<SignUp />} />
+                        <Route path='/login' element={<Login />} />
+                    </Routes> */}
+                <ClerkProviderWithRoutes />
                 <Footer />
             </BrowserRouter>
         </div>
