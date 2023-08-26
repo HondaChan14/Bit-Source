67950551c5d6c211048c3a1415f890661c2c1354
HondaChan14 <92832561+HondaChan14@users.noreply.github.com>
2023-08-26 15:15:56 -0400

    feat+refactor Addition of Morgan and modularize fetching

--- server/controllers/issuesController.js
+++ server/controllers/issuesController.js
@@ -1,22 +1,45 @@
+require('dotenv').config();
 const fetch = require('node-fetch');
+const GITHUB_API_URL = process.env.GITHUB_API_URL;
+
+const fetchGitHubIssues = async () => {
+    // Fetch GitHub issues using the provided URL
+    const response = await fetch(GITHUB_API_URL);
+
+    // Check if the API request was successful
+    if (!response.ok){
+        throw new Error(`GitHub API request failed with status: ${info.status}`);
+    }
+    // Return response in JSON
+    return response.json();
+}
+
+// Filter out pull requests from the fetched issues
+const filterOutPullRequests = (items) => {
+    return items.filter((item) => !item.pull_request);
+};
+
+// Sort issues by creation date in descending order
+const sortIssuesByCreationDate = (issues) => {
+    return issues.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
+};
+
+// Filter out issues that are already assigned
+const filterUnassignedIssues = (issues) => {
+    return issues.filter((issue) => issue.assignee === null);
+};
 
 module.exports = {
     getIssues: async (req, res) => {
         try {
-            const info = await fetch(
-                'https://api.github.com/search/issues?q=label:"good+first+issue"+language:javascript+is:open+no:assignee&sort=created&order=desc&per_page=100'
-            );
-            const data = await info.json();
-            const issues = data.items.filter((item) => !item.pull_request);
-            const recentIssues = issues.sort(
-                (a, b) => new Date(b.created_at) - new Date(a.created_at)
-            );
-            const filteredIssues = recentIssues.filter(
-                (issue) => issue.assignee === null
-            );
-            res.json(filteredIssues);
+            // Fetch GitHub issues from the API
+            const data = await fetchGitHubIssues();
+            // Filter out UnassignedIssued - Sort by Creation Date - filter out pull requests
+            const filteredIssues = filterUnassignedIssues(sortIssuesByCreationDate(filterOutPullRequests(data.items)))
+            // Send the filtered issues as a JSON response
+            res.json(filteredIssues)
         } catch (error) {
-            console.error(`${error} is coming from issues.js`);
+            // Handle any errors that occurred during the process
             res.status(500).json({ error: 'Server error' });
         }
     },
