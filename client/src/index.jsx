047e033bca38d9c3acd165cc1f8c8e55a371a468
HondaChan14 <92832561+HondaChan14@users.noreply.github.com>
2024-01-02 17:21:44 -0500

    refactor: move ClerkProvider from App.jsx to index.jsx

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,12 +3,22 @@ import ReactDOM from 'react-dom/client';
 import Modal from 'react-modal';
 import App from './App';
 import './index.css';
+import { ClerkProvider } from '@clerk/clerk-react';
+
+// Import your publishable key
+const PUBLISHABLE_KEY = import.meta.env.VITE_REACT_APP_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+    throw new Error('Missing Publishable Key');
+}
 
 Modal.setAppElement('#root');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
+        <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+            <App />
+        </ClerkProvider>
     </React.StrictMode>
 );
